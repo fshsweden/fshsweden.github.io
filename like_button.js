4d9af24cc062a51cd7f84ed389193491c554ca16
Peter Andersson <fshsweden@hotmail.com>
2019-07-14 20:38:53 +0200

    wip

--- like_button.js
+++ like_button.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 const e = React.createElement;
 
@@ -10,16 +10,12 @@ class LikeButton extends React.Component {
 
   render() {
     if (this.state.liked) {
-      return 'You liked this.';
+      return "Of course you like React JS!!! Everyone does!";
     }
 
-    return e(
-      'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Like'
-    );
+    return e("button", { onClick: () => this.setState({ liked: true }) }, "Push if you like React JS!");
   }
 }
 
-const domContainer = document.querySelector('#like_button_container');
+const domContainer = document.querySelector("#like_button_container");
 ReactDOM.render(e(LikeButton), domContainer);
